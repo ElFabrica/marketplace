87505495d1e4c83cf56289c3e591bdf460656f2e
Arthur <Arthur.fabricyo@gmail.com>
2025-10-05 01:14:44 -0300

    configurações de bibliotecas finalizadas e rotas inicializadas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,8 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Redirect } from "expo-router"
 import { router } from "expo-router"
 
 export default function App() {
     return (
-        <View>
-            <Text>
-                Olá mundo
-            </Text>
-            <TouchableOpacity onPress={() => router.push("login")}>
-                <Text className="size-4 text-6xl">Ir para login</Text>
-            </TouchableOpacity>
-        </View>
+        <Redirect href={"/login"} />
     )
 }
--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Login() {
     return (
-        <View>
-            <Text>Login</Text>
+        <View className="flex-1 items-center justify-center">
+            <TouchableOpacity onPress={() => router.push("/register")}>
+                <Text>Registro</Text>
+            </TouchableOpacity>
         </View>
     )
 }
